cecb68f67639bd615ee7fb6c457ecd6ea97fe7f9
Hugo Martinez <72633652+hugo01andres@users.noreply.github.com>
2023-11-23 18:51:00 +0000

    Add play-list-header and play-list-body components

--- frontend/spotify/src/app/modules/home/home-routing.module.ts
+++ frontend/spotify/src/app/modules/home/home-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   },
   {
     path: 'favorites',
-    loadChildren: () => import('./../favorites/favorites.module').then(m => m.FavoritesModule)
+    loadChildren: () => import('@modules/favorites/favorites.module').then(m => m.FavoritesModule),
   },
   {
     path: 'history',
