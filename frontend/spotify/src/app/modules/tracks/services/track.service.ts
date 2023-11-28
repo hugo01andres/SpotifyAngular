0ab0a934b4f0daa880bbb60ec6272454bbbd73f2
Hugo Martinez <72633652+hugo01andres@users.noreply.github.com>
2023-11-28 00:01:57 +0000

    Creo una promesa y un mergeMap

--- frontend/spotify/src/app/modules/tracks/services/track.service.ts
+++ frontend/spotify/src/app/modules/tracks/services/track.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Observable, map, of } from 'rxjs';
+import { Observable, map, mergeMap, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { TrackModel } from '@core/models/tracks.model';
 @Injectable({
   providedIn: 'root'
 })
@@ -15,6 +16,17 @@ export class TrackService {
     
   }
 
+  private skipById(listTracks: TrackModel[], id: number):Promise<TrackModel[]>{
+    return new Promise((resolve, reject) => {
+      const listTemp = listTracks.filter((track: TrackModel) => track._id != id)
+      resolve(listTemp)
+    })
+  }
+
+  /**
+   * 
+   * @returns Devolver tracks
+   */
   getAllTracks$(): Observable<any> {
     return this.httpClient.get(`${this.URL}/tracks`)
     .pipe(
@@ -24,12 +36,20 @@ export class TrackService {
     )
   }
 
+  /**
+   *  {data} es lo mismo que poner dataRaw, solo que aqui lo mapeamos mas limpio
+   * @returns Devolver tracks aleatoriosy alrevez
+   */
   getAllRandom$(): Observable<any> {
     return this.httpClient.get(`${this.URL}/tracks`)
     .pipe(
-      map((dataRaw:any) => {
-        return dataRaw.data
-      })
+      //NOTE: Utilizamos la promesa paa hacer la lista sin el id 1
+      mergeMap(({ data }: any) => this.skipById(data,1)),
+
+      //NOTE: APLICAR FILTER que sean todas las canciones excepto la de id 1
+      // map((dataRevertida) =>{
+      //   return dataRevertida.filter((track: TrackModel) => track._id != 1)
+      // })
     )
   }
 
