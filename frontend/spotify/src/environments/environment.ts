c6561ba9f70e4a6237dab61272b0504991260dab
Hugo Martinez <72633652+hugo01andres@users.noreply.github.com>
2024-04-21 04:13:08 +0000

    Configuracion del proyecto

--- frontend/spotify/src/environments/environment.ts
+++ frontend/spotify/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    api: 'https://urban-broccoli-6w6gwjp994wh5w7r-3001.app.github.dev/api/1.0',
+    api: 'https://psychic-space-chainsaw-46476xr7xgq3qrp-3001.app.github.dev/api/1.0',
 }
 // TODO: Poner el servidor en publico
